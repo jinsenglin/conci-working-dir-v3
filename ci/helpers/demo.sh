9e8d86f87da6437b6391b8940a0cfe5f4bb17feb
Jim Lin <jimlintw922@gmail.com>
2017-03-05 15:34:31 +0800

    refactor demo code

--- ci/helpers/demo.sh
+++ ci/helpers/demo.sh
@@ -3,7 +3,7 @@
 source ../assets/demo.env
 
 fly -t conci l -c $CONCOURSE_URL
-fly -t conci e -c ../tasks/demo.yml -i root=../../
-fly -t conci sp -c ../pipelines/demo.yml -p demo -n
+secret=demo fly -t conci e -c ../tasks/demo.yml -i root=../../
+fly -t conci sp -c ../pipelines/demo.yml -l ../assets/demo.yml -p demo -n
 fly -t conci up -p demo 
 fly -t conci tj -j 'demo/demo job' -w
