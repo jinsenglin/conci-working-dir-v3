3e00b682daa65e4c19698bba12e41299c11db9af
Jim Lin <jimlintw922@gmail.com>
2017-03-05 17:29:14 +0800

    refactor demo code

--- ci/helpers/demo.sh
+++ ci/helpers/demo.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 source ../assets/demo.env
+echo -n Enter GitHub Password: ; read GITPASS
 
 fly -t conci l -c $CONCOURSE_URL
 secret=demo fly -t conci e -c ../tasks/demo.yml -i root=../../
-fly -t conci sp -c ../pipelines/demo.yml -l ../assets/demo.yml -p demo -n
+fly -t conci sp -c ../pipelines/demo.yml -l ../assets/demo.yml -v github-password=$GITPASS -p demo -n
 fly -t conci up -p demo 
 fly -t conci tj -j 'demo/demo job' -w
